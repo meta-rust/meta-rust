meta-rust/meta-rust

commit 5afc0cc471eef5c18bbce9bc54aed6110e7c55eb

cargo.bbclass: ensure extra RUSTFLAGS are properly passed
The RUSTFLAGS env var overrides the rustflags specified in the cargo
config so we need to take anything passed in the environment variable
and put it in the config.

classes/cargo.bbclass
@@ -49,14 +49,20 @@ replace-with = "local"
 registry = "https://github.com/rust-lang/crates.io-index"
 EOF
 
-    # We need to use the real Yocto linker and get the linker
-    # flags to it. Yocto has the concept of BUILD and TARGET
-    # and uses HOST to be the currently selected one. However
-    # LDFLAGS and TOOLCHAIN_OPTIONS are not prefixed with HOST
-    echo "[build]" >> ${CARGO_HOME}/config
-    echo "rustflags = ['-C', 'link-args=${LDFLAGS}${HOST_CC_ARCH}${TOOLCHAIN_OPTIONS}']" >> ${CARGO_HOME}/config
-    echo "[target.${RUST_HOST_SYS}]" >> ${CARGO_HOME}/config
-    echo "linker = '${HOST_PREFIX}gcc'" >> ${CARGO_HOME}/config
+	# We need to use the real Yocto linker and get the linker
+	# flags to it. Yocto has the concept of BUILD and TARGET
+	# and uses HOST to be the currently selected one. However
+	# LDFLAGS and TOOLCHAIN_OPTIONS are not prefixed with HOST
+	echo "[build]" >> ${CARGO_HOME}/config
+	echo "rustflags = [" >> ${CARGO_HOME}/config
+	echo "'-C'," >> ${CARGO_HOME}/config
+	echo "'link-args=${LDFLAGS}${HOST_CC_ARCH}${TOOLCHAIN_OPTIONS}'," >> ${CARGO_HOME}/config
+	for p in ${RUSTFLAGS}; do
+		printf "'%s'\n" "$p"
+	done | sed -e 's/$/,/' >> ${CARGO_HOME}/config
+	echo "]" >> ${CARGO_HOME}/config
+	echo "[target.${RUST_HOST_SYS}]" >> ${CARGO_HOME}/config
+	echo "linker = '${HOST_PREFIX}gcc'" >> ${CARGO_HOME}/config
 }
 
 # All the rust & cargo ecosystem assume that CC, LD, etc are a path to a single
@@ -75,6 +81,7 @@ CARGO_BUILD_FLAGS = "-v --target ${HOST_SYS} --release"
 # change if CARGO_BUILD_FLAGS changes.
 CARGO_TARGET_SUBDIR="${HOST_SYS}/release"
 oe_cargo_build () {
+	unset RUSTFLAGS
 	bbnote "cargo = $(which cargo)"
 	bbnote "rustc = $(which rustc)"
 	bbnote "${CARGO} build ${CARGO_BUILD_FLAGS} $@"

recipes-devtools/cargo/cargo.inc
@@ -67,6 +67,10 @@ SRC_URI = "\
 
 B = "${S}"
 
+# Can't use the default from rust-vars as it cause a compiler crash
+# the crate_hash option will cause the compiler to crash
+RUSTFLAGS = "-C rpath ${RUSTLIB}"
+
 PACKAGECONFIG ??= ""
 
 # Note: this does not appear to work very well due to our use of bitbake triples

commit 2667a3e3634b8f79c783aa40884603c12c290222

classes/cargo: fix cargo target for linker override
We don't use the native Rust triples but instead use the Yocto triples
so we need to provide this override properly. This typo was introduced
in 5afc0cc471.

classes/cargo.bbclass
@@ -58,7 +58,7 @@ EOF
 		printf "'%s'\n" "$p"
 	done | sed -e 's/$/,/' >> ${CARGO_HOME}/config
 	echo "]" >> ${CARGO_HOME}/config
-	echo "[target.${RUST_HOST_SYS}]" >> ${CARGO_HOME}/config
+	echo "[target.${HOST_SYS}]" >> ${CARGO_HOME}/config
 	echo "linker = '${RUST_TARGET_CCLD}'" >> ${CARGO_HOME}/config
 }
 